7c2f2d96c9fb302a71f16418be3046b217808934
Victoria <victoria@vm>
2025-09-07 22:40:23 +0100

    feat: update HTTP source

--- repos/repos/IoT-Listener/src/utils/subscribe/sources.rs
+++ repos/repos/IoT-Listener/src/utils/subscribe/sources.rs
@@ -31,8 +31,7 @@ pub fn subscribe_and_monitor_source(
     let url = match source_node.source() {
         Source::MASMonitor(mas_monitor) => mas_monitor.get_monitor_url(),
         Source::Mqtt(mqtt) => mqtt.host.clone(),
-        Source::Kafka(kafka) => kafka.bootstrap_servers().to_string(),
-        Source::Http(http) => http.endpoint().to_string(),
+        Source::Http(http) => http.endpoint.clone(),
     };
 
     subscribe_and_restart(
